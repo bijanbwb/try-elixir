0b2b60a092b29c1b8625725cfd1085516e87911b
Bijan <bijanbwb@gmail.com>
2016-07-02 08:33:00 -0400

    Initial commit with Calc module
    Learning to work with Elixir from iex and using calc.ex as a
    playground area.

---
+++ calc.ex
@@ -0,0 +1,58 @@
+defmodule Calc do
+
+  @moduledoc """
+  Functions for performing calculations.
+  """
+  @vsn 0.1
+
+  @doc """
+  Prints hello to user.
+  """
+
+  @spec hello(name) :: string()
+
+  def hello(name) do
+    "Hello " <> name
+  end
+
+  @doc """
+  Adds two numbers together.
+  """
+
+  @spec add(number(), number()) :: number()
+
+  def add(a, b) do
+    a + b
+  end
+
+  @doc """
+  Subtracts one number from another.
+  """
+
+  @spec sub(number(), number()) :: number()
+
+  def sub(a, b) do
+    a - b
+  end
+
+  @doc """
+  Increments a number by one.
+  """
+
+  @spec inc(number()) :: number()
+
+  def inc(a) do
+    a + 1
+  end
+
+  @doc """
+  Decrements a number by one.
+  """
+
+  @spec dec(number()) :: number()
+
+  def dec(a) do
+    a - 1
+  end
+
+end
