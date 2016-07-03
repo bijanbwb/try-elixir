e8386e43a90bffc9af2aae21c8d4870c06f1c60b
Bijan <bijanbwb@gmail.com>
2016-07-03 15:46:57 -0400

    Try out a macro

---
+++ double.ex
@@ -0,0 +1,26 @@
+defmodule Double do
+
+  @moduledoc """
+  Macro for doubling a number value.
+  """
+  @vsn 0.1
+
+  @doc """
+  Uses macro quote syntax to double a number.
+
+  Usage:
+  ```elixir
+  c("double.ex")
+  require Double
+  Double.double(10)
+  # => 20
+  ```
+  """
+  @spec double(number) :: number()
+  defmacro double(x) do
+    quote do
+      2 * unquote(x)
+    end
+  end
+
+end
