e05cfeedc0ecfac369c4fe95b4419eb0c1843e0b
Bijan <bijanbwb@gmail.com>
2016-07-03 00:00:59 -0400

    Bounce module

---
+++ bounce.ex
@@ -0,0 +1,29 @@
+defmodule Bounce do
+
+  @moduledoc """
+  Bounce messages with report function.
+  """
+  @vsn 0.1
+
+  @doc """
+  Recursively uses `report()` function to receive and print messages.
+
+  Usage:
+  ```elixir
+  pid = spawn(Bounce, :report, [])
+  send(pid, :hello)
+  # => Received :hello
+  # => :hello
+  ```
+  """
+  @spec report() :: atom()
+
+  def report do
+    receive do
+      msg -> IO.puts("Received #{msg}")
+      report()
+    end
+  end 
+
+end
+
