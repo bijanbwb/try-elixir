ae7c7d791e370a82c9d2372dc764f40e91ce6adb
Bijan <bijanbwb@gmail.com>
2017-01-10 17:03:04 -0500

    Add process function to Issues project

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -7,7 +7,9 @@ defmodule Issues.CLI do
   """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
 
   @doc """
@@ -18,7 +20,6 @@ defmodule Issues.CLI do
 
   Return a tuple of `{ user, project, count }`, or `:help` if help was given.
   """
-
   def parse_args(argv) do
     parse = OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
 
@@ -28,5 +29,15 @@ defmodule Issues.CLI do
       {_, [user, project], _} -> {user, project, @default_count}
       _ -> :help
     end
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
   end
 end
