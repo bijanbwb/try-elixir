9cd42e03e6630af9183fde79fed981d1f5aea434
Bijan <bijanbwb@gmail.com>
2017-01-12 17:34:22 -0500

    Fix warnings, start using converter

--- physics/lib/physics/rocketry.ex
+++ physics/lib/physics/rocketry.ex
@@ -18,16 +18,12 @@ defmodule Physics.Rocketry do
   def escape_velocity(planet) when is_map(planet) do
     planet
     |> calculate_escape()
-    |> convert_to_km()
+    |> Converter.to_km()
     |> rounded_to_nearest_tenth()
   end
 
   defp rounded_to_nearest_tenth(val) do
     Float.ceil(val, 1)
-  end
-
-  defp convert_to_km(val) do
-    val / 1000
   end
 
   defp calculate_escape(%{mass: mass, radius: radius}) do
