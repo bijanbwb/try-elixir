a0d99cc0c86ac2ed0502710e619e9c4a4b18eeea
Bijan <bijanbwb@gmail.com>
2017-01-12 17:27:13 -0500

    Complete exercise for mars and moon

--- physics/test/physics_test.exs
+++ physics/test/physics_test.exs
@@ -4,15 +4,21 @@ defmodule PhysicsTest do
 
   test "escape velocity of earth is correct" do
     result = Physics.Rocketry.escape_velocity(:earth)
-
     assert result == 11.2
   end
 
-  test "escape velocity of planet X is correct" do
-    result =
-      %{mass: 4.0e22, radius: 6.21e6}
-      |> Physics.Rocketry.escape_velocity()
+  test "escape velocity of mars is correct" do
+    result = Physics.Rocketry.escape_velocity(:mars)
+    assert result == 5.1
+  end
 
+  test "escape velocity of the moon is correct" do
+    result = Physics.Rocketry.escape_velocity(:moon)
+    assert result == 2.4
+  end
+
+  test "escape velocity of planet X is correct" do
+    result = %{mass: 4.0e22, radius: 6.21e6} |> Physics.Rocketry.escape_velocity()
     assert result == 1.0
   end
 end
