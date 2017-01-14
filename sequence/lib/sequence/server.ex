2588281f3ca92a231a1c7fab810ef21a35fc6896
Bijan <bijanbwb@gmail.com>
2017-01-14 10:32:58 -0500

    Add handle_cast to sequence server

--- sequence/lib/sequence/server.ex
+++ sequence/lib/sequence/server.ex
@@ -1,6 +1,8 @@
 defmodule Sequence.Server do
   use GenServer
 
+  # Callbacks
+
   @doc """
   Replies with the next number in the sequence.
 
@@ -16,5 +18,22 @@ defmodule Sequence.Server do
   """
   def handle_call(:next_number, _from, current_number) do
     {:reply, current_number, current_number + 1}
+  end
+
+  @doc """
+  Increment the current state with a number.
+
+  ## Example Usage
+
+  iex> {:ok, pid} = GenServer.start_link(Sequence.Server, 100)
+  iex> GenServer.call(pid, :next_number)
+  100
+  iex> GenServer.cast(pid, {:increment_number, 200})
+  :ok
+  iex> GenServer.call(pid, :next_number)
+  301
+  """
+  def handle_cast({:increment_number, delta}, current_number) do
+    {:noreply, current_number + delta}
   end
 end
