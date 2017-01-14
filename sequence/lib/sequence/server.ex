e2a19f1206bf03ca796f3f0bf4dcea9a32ed5e64
Bijan <bijanbwb@gmail.com>
2017-01-14 10:44:44 -0500

    Add API for Sequence GenServer

--- sequence/lib/sequence/server.ex
+++ sequence/lib/sequence/server.ex
@@ -1,5 +1,35 @@
 defmodule Sequence.Server do
+  @moduledoc """
+  GenServer Sequence example from Programming Elixir.
+
+  ## Example Usage
+
+  iex> Sequence.Server.start_link(100)
+  iex> Sequence.Server.next_number()
+  100
+  iex> Sequence.Server.next_number()
+  101
+  iex> Sequence.Server.increment_number(50)
+  :ok
+  iex> Sequence.Server.next_number()
+  152
+  """
+
   use GenServer
+
+  # External API
+
+  def start_link(current_number) do
+    GenServer.start_link(__MODULE__, current_number, name: __MODULE__)
+  end
+
+  def next_number() do
+    GenServer.call(__MODULE__, :next_number)
+  end
+
+  def increment_number(delta) do
+    GenServer.cast(__MODULE__, {:increment_number, delta})
+  end
 
   # Callbacks
 
