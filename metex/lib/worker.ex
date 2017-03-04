880ccb3289dcf87389ebaed513f175391a609520
Bijan <bijanbwb@gmail.com>
2017-03-03 22:43:58 -0500

    Metex GenServer example

--- metex/lib/worker.ex
+++ metex/lib/worker.ex
@@ -1,31 +1,44 @@
 defmodule Metex.Worker do
-  def loop do
-    receive do
-      {sender_pid, location} ->
-        send(sender_pid, {:ok, temperature_of(location)})
-      _ ->
-        IO.puts "don't know how to process this message"
-    end
+  use GenServer
 
-    loop()
+  ## Client API
+
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, opts)
   end
 
-  def temperature_of(location) do
-    result = url_for(location)
-             |> HTTPoison.get
-             |> parse_response
+  def get_temperature(pid, location) do
+    GenServer.call(pid, {:location, location})
+  end
 
-    case result do
+  ## Server API
+
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
+  def handle_call({:location, location}, _from, stats) do
+    case temperature_of(location) do
       {:ok, temp} ->
-        "#{location}: #{temp}°C"
-      :error ->
-        "#{location} not found"
+        new_stats = update_stats(stats, location)
+        {:reply, "#{temp}°C", new_stats)
+
+      _ ->
+        {:reply, :error, stats}
     end
+  end
+
+  ## Helper Functions
+
+  defp temperature_of(location) do
+    location
+    |> url_for
+    |> HTTPoison.get
+    |> parse_response
   end
 
   defp url_for(location) do
-    location = URI.encode(location)
-    "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{apikey()}"
+    "http://api.openweathermbap.org/data/2.5/weather?q=#{location}&APPID=#{apikey}"
   end
 
   defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
@@ -40,8 +53,7 @@ defmodule Metex.Worker do
 
   defp compute_temperature(json) do
     try do
-      temp = (json["main"]["temp"] - 273.15)
-             |> Float.round(1)
+      temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
       {:ok, temp}
     rescue
       _ -> :error
@@ -50,5 +62,14 @@ defmodule Metex.Worker do
 
   defp apikey() do
     "YOUR-API-KEY"
+  end
+
+  defp update_stats(old_stats, location) do
+    case Map.has_key?(old_stats, location) do
+      true ->
+        Map.update!(old_stats, location, &(&1 + 1))
+      false ->
+        Map.put_new(old_stats, location, 1)
+    end
   end
 end
